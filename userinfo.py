b9a90191b1c8701fb5178966df388914b917aadc
ItsHenryF <63514033+ItsHenryF@users.noreply.github.com>
2020-07-03 17:35:56 -0500

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -1,3 +1,4 @@
+# Never Show This Information To Anybody, Your Twitch OAuth Token is Used to Connect to Your Twitch Account!
 # Get Your Oauth Token Here -> https://twitchapps.com/tmi/
 
 PASS = " " # Place Oauth Token Here in Entirety 
