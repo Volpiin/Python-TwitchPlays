e2a2b4e4feccf1dee7f1ab3bd1f2aaf751a55b13
ItsHenryF <63514033+ItsHenryF@users.noreply.github.com>
2020-07-12 01:02:24 -0500

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -1,13 +1,14 @@
 # Never Show This Information To Anybody, Your Twitch OAuth Token is Used to Connect to Your Twitch Account!
 # Get Your Oauth Token Here -> https://twitchapps.com/tmi/
 
-PASS = " " # Place Oauth Token Here in Entirety 
+PASS = " " # Place Oauth Token Here in Entirety ex) "oauth:AVeryLongStringofNumbersAndLetters"
 
 BOT = "TwitchBot"
 
 # Your Channel Name
 CHANNEL = ""
 
-# Your Bots Name or Your Channel Name
+# Your Bots Name or Your Channel Name 
+# This makes the program ignore inputs from this user
 OWNER = ""
 OWNER = OWNER
