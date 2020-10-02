08dedbeff90712e50ffec2c5dd8515c5bc15ab72
ItsHenryF <63514033+ItsHenryF@users.noreply.github.com>
2020-10-02 13:34:02 -0500

    Update TwitchController.py

--- TwitchController.py
+++ TwitchController.py
@@ -11,6 +11,14 @@ import pyautogui
 import keyboard
 import mouse
 import pydirectinput
+
+global special_char
+global capital_char
+global command_cooldown
+command_cooldown = []
+special_char = ['!','@',"#","$","%","^","&","*","(",")","?",]
+capital_char = ['A',"B",'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
 message = ' '
 user = ' '
 
@@ -40,29 +48,14 @@ def program():
                 "JOIN #" + CHANNEL + "\n").encode())
 
 
-
+    
     #Example of Twitch Bot Commands Setup
-    def commands():
-        global message
-        global user
-        while True:
-
-
-            if message.lower() == '!taco':
-                sendMessage(irc, "It's Taco Time!")
-                message = ''
-                return
-
-            else:
-                message = ''
-                return
-
-# This is Where you make Game Control Commands 
+    
     def gamecontrol():
         global message
         global user
 
-        global PressAndHoldKey
+
         def PressAndHoldKey(key, seconds):
             keyboard.press(key)
             time.sleep(seconds)
@@ -81,24 +74,6 @@ def program():
         def ReleaseKey(key):
             keyboard.press(key)
 
-        global PressAndHoldKey
-        def PressAndHoldKey(key, seconds):
-            keyboard.press(key)
-            time.sleep(seconds)
-            keyboard.release(key)
-
-        def PressAndHold2Key(key1,key2,seconds): #If You Need More Keys Just continue The pattern, create another def add key3
-            keyboard.press(key1)
-            keyboard.press(key2)
-            time.sleep(seconds)
-            keyboard.release(key1)
-            keyboard.release(key2)
-
-        def HoldKey(key):
-            keyboard.press(key)
-
-        def ReleaseKey(key):
-            keyboard.press(key)
         def MouseClick(key,seconds):#key means left or right for this one
             mouse.press(button = key)
             #Mouse has a timer otherwise left click may not get registerd in some games it can be 0.1 seconds
@@ -118,16 +93,15 @@ def program():
 
 
         while True:
+            #Minecraf
+
 
 
             #left click is what people would type in chat to make this command happen
             #MouseClick calls the function up above to make it happen. it required two inputs, the button (left or right) to be pressed, and the time to be pressed. 
             if message.lower() == 'left click':
-                chance = ActionChance(1,2)
-                if chance == 2:
-                    break
+
                 MouseClick('left',2)
-                message = ''
                 return
             
             #Twitch chat would need to type forward or w to make this command happen 
@@ -137,7 +111,6 @@ def program():
                 if ActionChance(1,10) % 2 == 0:
                     break
                 PressAndHoldKey('w',5)
-                message=''
                 return
 
             if message.lower() == 'back' or message.lower() == 's':
@@ -145,27 +118,22 @@ def program():
                 if ActionChance(4,5) != 5:
                     break
                 PressAndHoldKey('s',2)
-                message = ''
                 return
 
             if message.lower() == 'left' or message.lower() == 'port' or message.lower() == 'a':
-                PressAndHoldKey('a',2)
-                message = ''
+                PressAndHoldKey('a',10)
                 return
 
             if message.lower() == 'right' or message.lower() == 'starboard' or message.lower() == 'd':
                 PressAndHoldKey('d',2)
-                message = ''
                 return
 
             if message.lower() == 'jump':
                 PressAndHoldKey('space',2)
-                message = ''
                 return
 
             if message.lower() == 'crouch':
                 PressAndHoldKey('shift',2)
-                message = ''
                 return
 
             if message.lower() == 'run' or message.lower() == 'sprint': 
@@ -174,13 +142,13 @@ def program():
                 HoldKey('control')
                 PressAndHoldKey('w',2)
                 ReleaseKey('control')
-                message = ''
+                
                 return
-
+#********************************************************************
             #You may need to experiment with turning angels and time. This turns you about 90° to the right in MC
             if message.lower() == 'turn right':
                 MouseTurn(60,0,1)
-                message = ''
+                
                 return
             
             else:
@@ -253,15 +221,8 @@ def program():
                     user = getUser(line)
                     message = getMessage(line)
                     print(user + " : " + message)
-                    running = (gamecontrol(), commands())
-                                        
+                    t2 = threading.Thread(target = gamecontrol)
+                    t2. start()       
         joinchat()
-        
-        
-        
     t1 = threading.Thread(target = twitch)
     t1.start()
-    t2 = threading.Thread(target = gamecontrol)
-    t2. start()
-    t3 = threading.Thread(target = commands)
-    t3.start()
